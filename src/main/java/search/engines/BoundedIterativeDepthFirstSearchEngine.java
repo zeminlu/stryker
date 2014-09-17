027fa3d836b85a367772f8db46a78202c2a7a68c
Nazareno Aguirre <naguirre@gmail.com>
2014-09-17 13:13:54 -0300

    Iterative bounded depth first search implemented. Tests for this engine were added.

--- src/main/java/search/engines/BoundedIterativeDepthFirstSearchEngine.java
+++ src/main/java/search/engines/BoundedIterativeDepthFirstSearchEngine.java
@@ -1,14 +1,17 @@
 package search.engines;
 /**
  * Title:        DepthFirstEngine<p>
- * Description:  Class DepthFirstEngine implements a depth-first search strategy
-                 which can be used with any instance of AbstractSearchProblem. <p>
+ * Description:  Class BoundedIterativeDepthFirstSearchEngine implements a bounded depth-first search strategy
+                 iteratively. It can be used with any instance of AbstractSearchProblem. <p>
  * @author Nazareno Aguirre
- * @version 0.3
+ * @version 0.1
  */
 
 
 import java.util.*; // necessary due to the use of lists.
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import search.AbstractSearchProblem;
 import search.State;
@@ -18,7 +21,7 @@ public class BoundedIterativeDepthFirstSearchEngine<S extends State, Problem ext
 	private int visited;
 	private int bound = 3; 
 	
-	private Stack<State> opened;
+	private Stack<Pair<S, Integer>> opened; // stores opened states and their corresponding depth.
 
 	/** 
 	 * Constructor for class DepthFirstEngine.  
@@ -28,7 +31,7 @@ public class BoundedIterativeDepthFirstSearchEngine<S extends State, Problem ext
 	public BoundedIterativeDepthFirstSearchEngine() {
 		super();
 		visited = 0;
-		opened = new Stack<State>();
+		opened = new Stack<Pair<S, Integer>>();
 	}
 
 	/** 
@@ -43,63 +46,29 @@ public class BoundedIterativeDepthFirstSearchEngine<S extends State, Problem ext
 		super(p);
 		if (p==null) throw new IllegalArgumentException("creating engine on a null problem");
 		visited = 0;
-		opened = new Stack<State>();
+		opened = new Stack<Pair<S,Integer>>();
 	}
 
-
 	/** 
-	 * Starts the search for successful states for problem, following a 
-	 * depth-first strategy.
-	 * @return true iff a successful state is found.
-	 * @pre. problem!=null.
-	 * @post. the search is performed, the visited are stored in
-	 * list visited, the path in list path, and true is returned iff a       
-	 * successfull state is found.	 
-	 */
-	public boolean performSearch() {
-		if (this.problem==null) throw new IllegalStateException("initiating search on a null problem");
-		// we get the initial state
-		S initialState = problem.initialState();
-
-		// now we call a recursive method implementing depth-first
-		int depth = bound;
-		opened.clear();
-		opened.push(initialState);
-		
-		return false;
-
-	} 
-
-	/** 
-	 * Method that performs the search implementing a depth-first visit 
-	 * recursively.
-	 * @return true iff a successful state is found.
-	 * @param s is the state from which dfs is performed
-	 * @param depth is the maximum depth to perform dfs (bounded dfs)
-	 * @pre. s != null && depth>=0
-	 * @post. the search is performed, the visited are stored in
-	 * list visited, the path in list path, and true is returned iff a       
-	 * successful state is found.	 
+	 * Constructor for class DepthFirstEngine.
+	 * @param p is the search problem associated with the engine
+	 * being created.
+	 * @param maxDepth is the maximum depth to be explored in the bounded dfs.
+	 * @pre. p!=null.
+	 * @post. A reference to p is stored in field problem. Lists visited and
+	 * path are initialised as empty.
 	 */	
-	private boolean iterativeDepthFirst(S s, int depth) {
-		return false;
-	} // end of recursiveDepthFirst
+	public BoundedIterativeDepthFirstSearchEngine(Problem p, int maxDepth) {
+		super(p);
+		if (p==null) throw new IllegalArgumentException("creating engine on a null problem");
+		if (maxDepth<0) throw new IllegalArgumentException("invalid max depth");
+		visited = 0;
+		bound = maxDepth;
+		opened = new Stack<Pair<S,Integer>>();
+	}
 
-
-
-
-	/** 
-	 * Reports information regarding a previously executed search.   
-	 * @pre. performSearch() has been executed and finished.
-	 * @post. A report regarding the search is printed to standard output.
-	 * This report consists of the length of the path obtained, and the number
-	 * of visited states.
-	 */    
-	public String report() {
-		return("Number of visited states: "+visited);
-
-	} // end of report()
-
+	
+	
 	/**
 	 * Sets the maximum depth, or bound, for the bounded depth first search
 	 * @param depth is new depth to be used for bounded DFS.
@@ -107,5 +76,70 @@ public class BoundedIterativeDepthFirstSearchEngine<S extends State, Problem ext
 	public void setMaxDepth(int depth) {
 		this.bound = depth;
 	}
+
+	/** 
+	 * Starts the search for successful states for problem, following a 
+	 * bounded depth-first strategy.
+	 * @return true iff a successful state is found in depth smaller than bound.
+	 * @pre. problem!=null.
+	 * @post. the bounded dfs search is performed, and the result of the search is returned. 	 
+	 */
+	public boolean performSearch() {
+		if (this.problem==null) throw new IllegalStateException("initiating search on a null problem");
+		// we get the initial state
+		S initialState = problem.initialState();
+		// we initialise the stack of opened states
+		opened.clear();
+		opened.push(new ImmutablePair<S,Integer>(initialState, 0));
+		// we initiate the search
+		return iterativeDepthFirst();
+	} 
+
+	/** 
+	 * Method that performs the search implementing a depth-first visit 
+	 * recursively.
+	 * @return true iff a successful state is found within provided bounds.
+	 * @pre. bound>=0 && opened!=null && problem!=null
+	 * @post. the bounded dfs is performed, and true is returned iff a successful state is found.
+	 */	
+	private boolean iterativeDepthFirst() {
+		if (opened==null) throw new IllegalStateException("calling iterative dfs on a null stack of opened states");
+		boolean found = false;
+		while (!opened.isEmpty() && !found) {
+			Pair<S,Integer> current = opened.pop();
+			S currState = current.getLeft();
+			int currDepth = current.getRight();
+			if (currDepth<=this.bound) {
+				// state is within bounds. It must be treated.
+				visited++;
+				if (problem.success(currState)) {
+					found = true;
+				}
+				else {
+					// we only push childreno of curr if curr is not at the
+					// last level to treat
+					if (currDepth<this.bound) {
+						for (S s: problem.getSuccessors(currState)) {
+							Pair<S, Integer> child = new ImmutablePair<S, Integer>(s, currDepth+1);
+							opened.push(child);
+						}						
+					}
+				}
+			}
+		}
+		return found;
+	} 
+
+	/** 
+	 * Reports information regarding a previously executed search.   
+	 * @pre. performSearch() has been executed and finished.
+	 * @post. A report regarding the search is shown as a string.
+	 * This report consists simply of the number of visited states.
+	 */    
+	public String report() {
+		return("Number of visited states: "+visited);
+
+	}
+
 
 } 
---
+++ src/test/java/search/engines/BoundedIterativeDepthFirstSearchEngineTests.java
@@ -0,0 +1,191 @@
+package search.engines;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+import search.*;
+/**
+ * Unit tests for search engine BoundedIterativeDepthFirstSearchEngine.
+ * @author aguirre
+ *
+ */
+public class BoundedIterativeDepthFirstSearchEngineTests {
+
+	/**
+	 * IntState is a simple search state represented simply as an integer number.
+	 * @author aguirre
+	 *
+	 */
+	public class IntState implements State {
+		
+		private int value;  // value of current state
+		
+		/**
+		 * Constructor of IntState. Sets value of state to the parameter
+		 * @param value is the value to set the state.
+		 */
+		public IntState(int value) {
+			this.value = value;
+		}
+
+		/**
+		 * Returns the value of the current state.
+		 * @return value of the current state.
+		 */
+		public int getValue() {
+			return value;
+		}
+		
+		/**
+		 * Indicates whether two states are equal or not.
+		 */
+		public boolean equals(State other) {
+			if (!(other instanceof IntState)) return false;
+			IntState otherIntState = (IntState) other;
+			return (this.value==otherIntState.value);
+		}
+		
+	}
+	
+	/**
+	 * Simple search problem defined for testing purposes.
+	 * The search problem simply consists in searching for an integer
+	 * value in a state space of integers. Initial state is 0.
+	 * Successors of a state i are (i+1) and (i+2). A state is successful if it's
+	 * equal to the goal of the problem (set with the constructor).
+	 * @author aguirre
+	 *
+	 */
+	public class FindIntProblem implements AbstractSearchProblem<IntState> {
+		
+		private int goal; // value to search for.
+
+		/**
+		 * Constructor for FindIntProblem. Sets the value to search for.
+		 * @param goal is the value to search for.
+		 */
+		public FindIntProblem(int goal) {
+			this.goal = goal;
+		}
+
+		/**
+		 * Returns the initial state of the problem. In this case
+		 * the initial state is zero.		
+		 */
+		public IntState initialState() {
+			return new IntState(0);
+		}
+
+		/**
+		 * Returns the successors of an initial state. In this case, the successors
+		 * of a value i are (i+1) and (i+2).
+		 */
+		public List<IntState> getSuccessors(IntState s) {
+			List<IntState> children = new LinkedList<IntState>();
+			children.add(new IntState(s.getValue()+1));
+			children.add(new IntState(s.getValue()+2));
+			return children;
+		}
+
+		/**
+		 * It decides whether a state is successful or not, comparing it with
+		 * the goal of the problem. Returns true iff s is the goal
+		 */
+		public boolean success(IntState s) {
+			return (this.goal==s.getValue());
+		}
+		
+		/**
+		 * It sets the goal of the problem.
+		 * @param goal is the new value to search for.
+		 */
+		public void setGoal(int goal) {
+			this.goal = goal;
+		}
+		
+	}
+	
+	/**
+	 * Search for 0 up to depth 0, starting from 0. Should succeed!
+	 */
+	@Test
+	public void testBoundedDfsWithNilBoundsSuccess() {
+		FindIntProblem problem = new FindIntProblem(0);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(0);
+		assertTrue("number 0 should be found in depth 0", engine.performSearch());
+	}
+
+	/**
+	 * Search for 1 up to depth 0, starting from 0. Should fail!
+	 */
+	@Test
+	public void testBoundedDfsWithNilBoundsFail() {
+		FindIntProblem problem = new FindIntProblem(1);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(0);
+		assertFalse("number 1 should not be found in depth 0", engine.performSearch());
+	}
+
+	/**
+	 * Search for 2 up to depth 1, starting from 0. Should succeed!
+	 */
+	@Test
+	public void testBoundedDfsSuccessWithinBounds() {
+		FindIntProblem problem = new FindIntProblem(2);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(1);
+		assertTrue("number 2 should be found in depth 1", engine.performSearch());
+	}
+
+	/**
+	 * Search for 2 up to depth 0, starting from 0. Should fail!
+	 */
+	@Test
+	public void testBoundedDfsSuccessOutsideBounds() {
+		FindIntProblem problem = new FindIntProblem(2);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(0);
+		assertFalse("number 2 should be not be found up to depth 0", engine.performSearch());
+	}
+
+	/**
+	 * Search for 20 up to depth 10, starting from 0. Should succeed!
+	 */
+	@Test
+	public void testBoundedDfsSuccessInsideBiggerBounds() {
+		FindIntProblem problem = new FindIntProblem(20);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(10);
+		assertTrue("number 20 should be found (search up to depth 10)", engine.performSearch());
+	}
+
+	/**
+	 * Search for 21 up to depth 10, starting from 0. Should fail!
+	 */
+	@Test
+	public void testBoundedDfsSuccessOutsideBiggerBounds() {
+		FindIntProblem problem = new FindIntProblem(21);
+		BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem> engine = 
+				new BoundedIterativeDepthFirstSearchEngine<IntState, FindIntProblem>();
+		engine.setProblem(problem);
+		engine.setMaxDepth(10);
+		assertFalse("number 21 should not be found (search up to depth 10)", engine.performSearch());
+	}
+	
+	
+}
